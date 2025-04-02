821978c04db1c3312d4796314af61eae8b63b428
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2025-04-02 19:51:25 +0200

    homepage draft

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+	return (
+		<div className="root-layout">
+			<nav>
+				<Link href="/" className="flex item-center gap-2">
+					<Image src="/logo.svg" alt="interviewly" width={450} height={300} />
+				</Link>
+			</nav>
+			{children}
+		</div>
+	);
 };
 
 export default RootLayout;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,50 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 const page = () => {
-  return <div>Home page</div>;
+	return (
+		<>
+			<section className="card-cta">
+				<div className="flex flex-col gap-6 max-w-lg">
+					<h2>
+						Get Interview-Ready with Interviewly. <br />
+						An AI powered platform that helps you prepare for your next
+						interview.
+					</h2>
+					<p className="text-lg">
+						Practice on real interview questions, get feedback on your answers,
+						and learn from others. All in one place.
+					</p>
+
+					<Button asChild className="btn-primary max-sm:w-full">
+						<Link href="/interview">Start a Practice Interview</Link>
+					</Button>
+				</div>
+				<Image
+					src="/robot.png"
+					alt="robot"
+					width={400}
+					height={400}
+					className="max-sm:hidden"
+				/>
+			</section>
+
+			<section className="flex flex-col gap-6 mt-8">
+				<h2>Your Interviews</h2>
+				<div className="interviews-section">
+					<p>You haven&apos;t taken any interviews yet</p>
+				</div>
+			</section>
+
+			<section className="flex flex-col gap-6 mt-8">
+				<h2>Take an Interview</h2>
+				<div className="interviews-section">
+					<p>There are no interviews available</p>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default page;
