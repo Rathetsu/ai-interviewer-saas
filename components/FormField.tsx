2701e4d27f6a067475e8d909a985489bfefec719
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2025-04-05 22:52:32 +0200

    Show/Hide password in auth forms

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
 
 import {
@@ -8,6 +10,9 @@ import {
 	FormMessage,
 } from "./ui/form";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface FormFieldProps<T extends FieldValues> {
 	control: Control<T>;
@@ -25,26 +30,57 @@ const FormField = <T extends FieldValues>({
 	placeholder,
 	description,
 	type = "text",
-}: FormFieldProps<T>) => (
-	<Controller
-		name={name}
-		control={control}
-		render={({ field }) => (
-			<FormItem>
-				<FormLabel className="label">{label}</FormLabel>
-				<FormControl>
-					<Input
-						className="input"
-						{...field}
-						placeholder={placeholder}
-						type={type}
-					/>
-				</FormControl>
-				{description && <FormDescription>{description}</FormDescription>}
-				<FormMessage />
-			</FormItem>
-		)}
-	/>
-);
+}: FormFieldProps<T>) => {
+	const [showPassword, setShowPassword] = useState(false);
+
+	const togglePassword = () => {
+		setShowPassword(!showPassword);
+	};
+
+	return (
+		<Controller
+			name={name}
+			control={control}
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel className="label">{label}</FormLabel>
+					<FormControl>
+						<div className="relative">
+							<Input
+								className="input"
+								{...field}
+								placeholder={placeholder}
+								type={
+									type === "password"
+										? showPassword
+											? "text"
+											: "password"
+										: type
+								}
+							/>
+							{type === "password" && (
+								<Button
+									type="button"
+									variant="ghost"
+									size="icon"
+									className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+									onClick={togglePassword}
+								>
+									{showPassword ? (
+										<EyeOff className="h-4 w-4 text-muted-foreground" />
+									) : (
+										<Eye className="h-4 w-4 text-muted-foreground" />
+									)}
+								</Button>
+							)}
+						</div>
+					</FormControl>
+					{description && <FormDescription>{description}</FormDescription>}
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+};
 
 export default FormField;
